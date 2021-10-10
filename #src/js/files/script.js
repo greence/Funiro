b0f42eef6cf83663119360753185400452ceb616
Greence <62187174+greence@users.noreply.github.com>
2021-10-10 23:49:17 +0300

    11

---
+++ #src/js/files/script.js
@@ -0,0 +1,39 @@
+
+window.onload = function () {
+	document.addEventListener("click", documentActions)
+
+	// Actions (делегирование события click)
+	function documentActions(e) {
+		const targetElement = e.target
+		if (window.innerWidth > 768 && isMobile.any()) {
+			if (targetElement.classList.contains('menu__arrow')) {
+				//_removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover")
+				targetElement.closest('.menu__item').classList.toggle('_hover')
+			}
+			if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+				_removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover")
+			}
+		}
+		if (targetElement.classList.contains('search-form__icon')) {
+			document.querySelector('.search-form').classList.toggle('_active')
+		} else if (!targetElement.closest('.search-form') && document.querySelector('.search-form._active')) {
+			document.querySelector('.search-form').classList.remove('_active')
+		}
+
+	}
+}
+
+
+
+		// let menuItem = document.querySelectorAll(".menu__arrow");
+
+		// for (let i = 0; i < menuItem.length; i++) {
+
+		// 	if (menuItem[i] === targetElement) {
+		// 		targetElement.closest('.menu__item').classList.toggle('_hover')
+
+		// 		console.log(targetElement.closest('.menu__item').nextSibling);
+		// 	} else {
+				// menuItem[i].previousElementSibling.classList.remove('_hover')
+				//menuItem[i].className = menuItem[i].className.remove("_hover");
+
