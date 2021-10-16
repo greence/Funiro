856896604d7d6bdc28767d9a02fe10a672a76bfe
Maria <greence34@gmail.com>
2021-10-16 16:36:39 +0300

    load more products finished

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -19,6 +19,10 @@ window.onload = function () {
 		} else if (!targetElement.closest('.search-form') && document.querySelector('.search-form._active')) {
 			document.querySelector('.search-form').classList.remove('_active')
 		}
+		if (targetElement.classList.contains('products__more')) {
+			e.preventDefault()
+			getProducts(targetElement)
+		}
 	}
 
 	//Header
@@ -34,6 +38,88 @@ window.onload = function () {
 
 	const headerObserver = new IntersectionObserver(callback);
 	headerObserver.observe(headerElement);
+
+	//Load more products 
+	async function getProducts(button) {
+		if (!button.classList.contains('_hold')) {
+			button.classList.add('_hold')
+			const file = 'json/products.json'
+			let response = await fetch(file, {
+				method: "GET",
+			})
+			if (response.ok) {
+				let result = await response.json()
+				console.log(result)
+				loadProducts(result)
+				button.classList.remove('_hold')
+				button.remove()
+			}
+			else {
+				alert('Error!')
+			}
+		}
+	}
+
+	function loadProducts(data) {
+		const productsItems = document.querySelector('.products__items')
+		data.products.forEach(item => {
+			const productId = item.id
+			const productUrl = item.url
+			const productImage = item.image
+			const productTitle = item.title
+			const productText = item.text
+			const productPrice = item.price
+			const productOldPrice = item.priceOld
+			const productShareUrl = item.shareUrl
+			const productLikeUrl = item.likeUrl
+			const productLabels = item.labels
+
+			let productTemplateLabelsContent = ''
+			if (productLabels) {
+				productLabels.forEach(labelItem => {
+					productTemplateLabelsContent += `<div class="item-product__label item-product__label_${labelItem.type}">${labelItem.value}</div>`;
+				});
+			}
+
+			let template = `
+			<article data-id="${productId}" class="products__item item-product">
+							<div class="item-product__labels">
+								${productTemplateLabelsContent}
+							</div>
+							<a href="${productUrl}" class="item-product__image _ibg">
+								<img src="img/products/${productImage}" alt="${productTitle}-image">
+							</a>
+							<div class="item-product__body">
+								<div class="item-product__content">
+									<h5 class="item-product__title">
+										${productTitle}
+									</h5>
+									<div class="item-product__text">
+										${productText}
+									</div>
+									<div class="item-product__prices">
+										<div class="item-product__price">
+											${productPrice}
+										</div>
+										<div class="item-product__price_old">
+											${productOldPrice}
+										</div>
+									</div>
+									<div class="item-product__actions actions-product">
+										<div class="actions-product__body">
+											<a href="#" class="actions-product__button btn btn_white">Add to cart</a>
+											<a href="${productShareUrl}" class="actions-product__link _icon-share">Share</a>
+											<a href="${productLikeUrl}" class="actions-product__link _icon-favorite">Like</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</article>
+			`
+
+			productsItems.insertAdjacentHTML('beforeend', template);
+		});
+	}
 }
 
 
